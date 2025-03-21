4c0d299559d760153a608129448347ad1f2b6951
hanq11 <hanq11@fpt.edu.vn>
2025-03-21 10:24:51 +0700

    update

---
+++ TUTOR_PRO1041_SP24B2/src/tutor_pro1041_sp24b2/util/DbConnection.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutor_pro1041_sp24b2.util;
+import java.sql.*;
+/**
+ *
+ * @author Syn
+ */
+public class DbConnection {
+public static final String HOSTNAME = "localhost";
+    public static final String PORT = "1433";
+    public static final String DBNAME = "TUTOR_PRO1041";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "123456";
+
+    /**
+     * Get connection to MSSQL Server
+     *
+     * @return Connection
+     */
+    public static Connection getConnection() {
+
+        // Create a variable for the connection string.
+        String connectionUrl = "jdbc:sqlserver://" + HOSTNAME + ":" + PORT + ";"
+                + "databaseName=" + DBNAME +";encrypt=true;trustServerCertificate=true;";
+
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection(connectionUrl, USERNAME, PASSWORD);
+        } // Handle any errors that may have occurred.
+        catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace(System.out);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getConnection());
+    }
+}
